853243abe59e0be054fb93b0f327b2c817930124
ahmar35 <muhammadahmar35@gmail.com>
2023-07-25 20:07:49 +0530

    user table using mongoose

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 /* const mongoConnect=require('./util/database').mongoConnect
  */
-/* const User=require('./models/user')
- */
+const User=require('./models/user')
+
 const mongoose=require('mongoose')
 const errorController = require('./controllers/error');
 
@@ -22,16 +22,16 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-/* app.use((req, res, next) => {
-  User.findById('64bd1099cfdf34c623472f28')
+app.use((req, res, next) => {
+  User.findById('64bfcd460332f43814083d66')
     .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
+      req.user = user
       next();
     })
     .catch(err => console.log(err));
     
 });
- */
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
@@ -40,6 +40,14 @@ app.use(errorController.get404);
 
 mongoose.connect('mongodb+srv://muhammadahmar35:Rahil123@cluster0.wsvyht3.mongodb.net/shop?retryWrites=true&w=majority')
 .then(result =>{
+ /*  const user=new User({
+    name:'ash',
+    email:'ash@.c',
+    cart:{
+      items:[]
+    }
+  })
+  user.save() */
   app.listen(3000)
 
 })
